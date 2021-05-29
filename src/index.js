1b9a356396e1604d0637bbca1c918e212f3d3f8f
FranciscAndrade <mariafranciscandrade@gmail.com>
2021-05-29 11:15:11 +0100

    inProgress

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-alert
+function displayTemperature (response) 
+{
+
+    console.log(response.data);
+    console.log(response.data.main.temp)
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    temperatureElement.innerHTML = Math.round (response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+}
+
+let apiKey = "3ae08bc66561a3f101f2a9b8b4f158c4";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
+
