9fa27e9e0c04d80ed55e357ed4b525c1dc5f20d1
FranciscAndrade <mariafranciscandrade@gmail.com>
2021-05-30 12:20:23 +0100

    fixingF|Cbug

--- src/index.js
+++ src/index.js
@@ -30,7 +30,14 @@ function displayTemperature (response)
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon")
-    temperatureElement.innerHTML = Math.round (response.data.main.temp);
+
+
+
+celsiusTemperature = response.data.main.temp;
+
+
+
+    temperatureElement.innerHTML = Math.round (celsiusTemperature);
     cityElement.innerHTML = response.data.name;
     description.innerHTML = response.data.weather[0].description;
     humidity.innerHTML = response.data.main.humidity;
@@ -63,13 +70,14 @@ function handleSubmit(event)
 function displayFahrenheitTemperature(event) 
 {
   event.preventDefault();
-  let fahrenheitTemperature = (14 * 9 / 5) + 32;
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = fahrenheitTemperature;
+  let fahrenheitTemperature = (celsiusTemperature * 9 / 5) + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
-search("New York");
 
+
+let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
@@ -77,3 +85,5 @@ form.addEventListener("submit", handleSubmit);
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
+
+search("New York");
