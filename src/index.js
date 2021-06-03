4c24cb09874dad70c3dde71348661446ba56a65d
FranciscAndrade <mariafranciscandrade@gmail.com>
2021-06-03 12:28:41 +0100

    WeekForecast

--- src/index.js
+++ src/index.js
@@ -24,27 +24,45 @@ function formatDate(timestamp) {
    return `${day}:${hours}:${minutes}`;
 }
 
+
+function formatDay(timestamp) {
+  let date = new Date (timestamp * 1000)
+  let day = date.getDay();
+  let days = 
+  ["Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"]
+
+    return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily)
+  let forecast = response.data.daily;
+
+
   let forecastElement = document.querySelector("#forecast");
-
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6)
+
     forecastHTML =
       forecastHTML +
       `
       <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
-          src="http://openweathermap.org/img/wn/50d@2x.png"
+          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
           alt=""
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 18° </span>
-          <span class="weather-forecast-temperature-min"> 12° </span>
+          <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
+          <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}° </span>
         </div>
       </div>
   `;
@@ -53,7 +71,6 @@ function displayForecast(response) {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-
 
 function getForecast(coordinates) 
 {
@@ -71,9 +88,7 @@ function displayTemperature (response)
     let humidity = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
-    let iconElement = document.querySelector("#icon")
-
-
+    let iconElement = document.querySelector("#icon");
 
     celsiusTemperature = response.data.main.temp;
     temperatureElement.innerHTML = Math.round (celsiusTemperature);
