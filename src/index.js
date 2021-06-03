fff0f0f088509476ccf92264982f34d5d6240166
FranciscAndrade <mariafranciscandrade@gmail.com>
2021-06-03 09:15:42 +0100

    someMoreJs

--- src/index.js
+++ src/index.js
@@ -24,7 +24,8 @@ function formatDate(timestamp) {
    return `${day}:${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily)
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -54,7 +55,13 @@ function displayForecast() {
 }
 
 
-
+function getForecast(coordinates) 
+{
+console.log(coordinates);
+let apiKey = "3ae08bc66561a3f101f2a9b8b4f158c4";
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature (response) 
 {
@@ -80,6 +87,9 @@ function displayTemperature (response)
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+getForecast(response.data.coord);
+
 }
 
 
@@ -132,4 +142,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
-displayForecast(); 
